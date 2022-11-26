52e316ea5dcea15d174899d6d892207a2cb78c7e
airport117 <115542071+airport117@users.noreply.github.com>
2022-11-26 21:53:17 +0800

    1

--- src/main/java/cn/itcast/hotel/pojo/HotelDoc.java
+++ src/main/java/cn/itcast/hotel/pojo/HotelDoc.java
@@ -1,5 +1,7 @@
 package cn.itcast.hotel.pojo;
 
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -12,6 +14,7 @@ import java.util.List;
 @Data
 @NoArgsConstructor
 public class HotelDoc {
+
     private Long id;
     private String name;
     private String address;
